7e8ed7a952bb81509f40f11a96f63ca0a06b40f6
h-wasi <akhsaikas@gmail.com>
2024-02-20 16:03:56 +0500

    cursor animation added

--- components/Catch.tsx
+++ components/Catch.tsx
@@ -1,8 +1,9 @@
 import { email } from "@/constant";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion, useMotionValue, useSpring } from "framer-motion";
 import useMouse from "@react-hook/mouse-position";
+import { redirect } from "next/navigation";
 
 function Catch() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +12,7 @@ function Catch() {
     stiffness: 500,
     damping: 28,
   };
+  const ref2 = useRef(null);
   const useVariants = (ref: React.MutableRefObject<null>) => {
     let mouseXPosition = 0;
     let mouseYPosition = 0;
@@ -27,21 +29,27 @@ function Catch() {
     }
     return {
       default: {
-        x: mouseXPosition,
-        y: mouseYPosition,
+        x: mouseXPosition > 0 ? mouseXPosition - 200 : mouseXPosition + 900,
+        y: mouseYPosition > 0 ? mouseYPosition - 100 : mouseYPosition + 300,
         transition: {
-          type: "spring",
-          mass: 0.6,
+          duration: 2.5,
         },
       },
     };
   };
+
+  const [cursorVariant, setCursorVariant] = useState("default");
+  const variants = useVariants(ref2);
+
   return (
     <div
       className="bg-slate-50 bg-cover border-b shadow-lg shadow-gray-700/20 border-slate-500/80 rounded-b-2xl grid-catch flex items-center justify-center max-md:hidden"
       id="catchme"
     >
-      <div className="h-screen rounded-xl border border-transparent bg-white bg-grid invert w-[94vw] flex items-center justify-end px-16 absolute right-0 overflow-hidden">
+      <div
+        className="h-screen rounded-xl border border-transparent bg-white bg-grid invert w-[94vw] flex items-center justify-end px-16 absolute right-0 overflow-hidden"
+        ref={ref2}
+      >
         <h1 className="absolute left-10 text-8xl xl:leading-[1.3] leading-[1] w-[55%] selection:bg-pink-400/50">
           Excited to<span className="text-violet-800"> code</span>,
           <span className="text-emerald-300"> create</span>, and
@@ -50,13 +58,10 @@ function Catch() {
           <span className="text-lime-400"> triumphs</span>.
         </h1>
         <motion.div
-          // animate={{
-          //   x: mouseXPosition,
-          //   y: mouseYPosition,
-          //   transition: { type: "spring", mass: 0.6 },
-          // }}
-          // transition={spring}
-          className="cursor absolute w-80 h-44 bg-graffiti bg-cover border border-gray-600 rounded-xl flex items-center justify-center invert"
+          animate={cursorVariant}
+          variants={variants}
+          transition={spring}
+          className="cursor absolute inset-0 w-80 h-44 bg-graffiti bg-cover border border-gray-600 rounded-xl flex items-center justify-center invert"
           onMouseEnter={() => setIsOpen(true)}
           onMouseLeave={() => setIsOpen(false)}
         >
@@ -67,8 +72,9 @@ function Catch() {
             Catch Me
           </h1>
           <Link
+            onDrag={() => redirect(`mailto:${email}`)}
+            onClick={() => redirect(`mailto:${email}`)}
             href={`mailto:${email}`}
-            target="_blank"
             className="text-white text-3xl font-bold w-60 text-center h-16 bg-black border hidden data-[state=open]:flex data-[state=open]:justify-center data-[state=open]:items-center border-gray-700 rounded-xl"
             data-state={isOpen ? "open" : "closed"}
           >
